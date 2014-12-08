86f71e7f914082022376e6da7959d70d8eaa3703
EBM26 <ebm2626@yahoo.com>
2014-12-07 17:02:57 -0800

    changes

--- RockPaper/index.js
+++ RockPaper/index.js
@@ -2,11 +2,14 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
 	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+} 
+else if(computerChoice <= 0.67) {
 	computerChoice = "paper";
-} else {
+} 
+else {
 	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+} 
+console.log("Computer: " + computerChoice);
 var compare = function(choice1, choice2){
     if (choice1 === choice2) {
     return "The result is a tie!";
@@ -40,8 +43,10 @@ var compare = function(choice1, choice2){
           else { 
               return "scissors wins";
           }
-    }
+    
     }
     };
 
-compare(userChoice, computerChoice);
+
+alert(compare(userChoice, computerChoice));
+
